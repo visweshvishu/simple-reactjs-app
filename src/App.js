a2702004c10eb58a2410dc72d3e795a42267fbf5
viswesh <visweshvishu125@gmail.com>
2024-10-24 22:49:56 +0530

    dff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-do
 class App extends Component {
   render() {
     console.log("Host URL"+process.env.PUBLIC_URL);
-console.log("hello just now in inst alled ");
+console.log("hello just now in inst al led ");
     return (
 
       <Router basename={process.env.PUBLIC_URL}>
