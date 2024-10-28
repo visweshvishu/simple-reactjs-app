fdaa18a9c830f936d65f3636d07d894f55ab2ca6
viswesh <visweshvishu125@gmail.com>
2024-10-28 21:17:15 +0530

    use functional

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Customers from './Customers';
+import { BrowserRouter as Router, Route, Redirect, Routes } from 'react-router-dom';
 
+const App = () => {
+  console.log("Host URL: " + process.env.PUBLIC_URL);
+  console.log("hello just it is installed");
 
-class App extends Component {
-  render() {
-    console.log("Host URdfL"+process.env.PUBLIC_URL);
-console.log("hello just it is installed" );
-    return (
-<div>
-<h1>jksndfkjndfjkndfnkjdfn</h1>
+  return (
+    <div>
+      <h1>jksndfkjndfjkndfnkjdfn</h1>
       <Router>
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simplegh React App</h1>
-        </header>
-          <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
-          </Switch>
-      </div>
-    </Router>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Simplegh React App</h1>
+          </header>
+          <Routes>
+            <Route exact path="/" element={<Redirect to="/customerlist" />} />
+            <Route path="/customerlist" element={<Customers />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
-    );
-  }
+  );
 }
 
 export default App;
