f15bb40a235d9008dcad1d604d9f5aacaf05e335
vesure <vesure@galaxe.com>
2024-10-29 15:48:19 +0530

    test again

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     console.log("Host URdfL"+process.env.PUBLIC_URL);
-console.log("hello it is installed" );
+console.log("hello it is viswesh installed" );
     return (
 <div>
 <h1>Simple react app</h1>
