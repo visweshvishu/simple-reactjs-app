c77e18a289ec08c0835dccb1856671047ba806d4
vesure <vesure@galaxe.com>
2024-10-29 11:45:31 +0530

    hjsd

--- src/App.js
+++ src/App.js
@@ -12,20 +12,7 @@ console.log("hello just it is installed" );
     return (
 <div>
 <h1>jksndfkjndfjkndfnkjdfn</h1>
-      {/* <Router>
-        <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simplegh React App</h1>
-        </header>
-          <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
-          </Switch>
-      </div>
-    </Router> */}
+  
     </div>
     );
   }
